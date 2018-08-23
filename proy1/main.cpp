9c2e35fbba5dfb559866e8031505170327bfab80
ALUMNO-N415 <ALUMNO-N415@UCSP.AULAB>
2018-08-23 09:50:12 -0500

    nuevos cambios

--- proy1/main.cpp
+++ proy1/main.cpp
@@ -4,8 +4,20 @@ using namespace std;
 
 int main()
 {
-    cout << "Hola Mundo!" << endl;
-    cout << "Me llamo Sebastian" << endl
+    /*cout << "Hola Mundo!" << endl;
+    cout << "Me llamo Sebastian" << endl;
+
+    short int a, b, c, d, e;
+    cout << "ingrese cinco numeros :" << endl;
+    cin >> a >> b >> c >> d >> e;
+    cout << "El promedio es :" << (a+b+c+d+e)/5.0 << endl;
+
+    int x = 	2147483647, y;
+    cout << "valores :";
+    cin >>  y;
+    cout << "la suma es:" << x+y;*/
+    float z = (2**0 + 2**1 + 2**3 + 2**4 + 2**5 + 2**6 + 2**7 + 2**8 + 2**9 + 2**10 + 2**11 + 2**12 + 2**13 + 2**14 + 2**15 + 2**16);
+    cout << z; endl
 
     return 0;
 }
